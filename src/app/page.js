5e74cbfeacfd26262b9e71781cf0f4793776cdeb
Devhell69 <paelalit@gmail.com>
2024-02-02 23:34:28 +0700

    Test TT

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello EcoPluse</h1>
+      <h2>Test</h2>
     </main>
   );
 }
