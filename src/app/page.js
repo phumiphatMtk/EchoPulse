9ef4863dee9de77146eaa27572e4ab58e20c06b0
Phumiphat Matakeaw <phumiphat.mtk@gmail.com>
2024-02-02 23:46:43 +0700

    create component card

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { CardComponent } from "@/shared/components";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello EcoPluse</h1>
-      <h2>Test</h2>
+      <div className="container">
+        <CardComponent />
+      </div>
     </main>
   );
 }
