a3c9823e864b40f9308a26fddc0131a6d58afcdc
Devhell69 <paelalit@gmail.com>
2024-02-06 04:28:03 +0700

    Update Component Activity

--- src/shared/components/Activity.js
+++ src/shared/components/Activity.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
+import Image from "next/image";
+import A1 from "../../assets/images/A1.jpg";
+import A2 from "../../assets/images/A2.jpg";
+import A3 from "../../assets/images/A3.jpg";
+import A4 from "../../assets/images/A4.jpg";
 
 const Activity = () => {
-  return (
-    <div>Activity</div>
-  )
-}
+  const swiperOptions = {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 3,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: { el: ".swiper-pagination", clickable: true },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+      clickable: true,
+    },
+    modules: [EffectCoverflow, Pagination, Navigation],
+    className: "swiper-container",
+  };
 
-export default Activity
+  const slideImages = [A1, A2, A3, A4];
+
+  return (
+    <div className="headerprocess">
+      <h1 className="text-center text-3xl mb-3">
+        <i className="fa-solid fa-gift gtnbtn"></i>กิจกรรม
+      </h1>
+      <Swiper {...swiperOptions}>
+        {slideImages.map((image, index) => (
+          <SwiperSlide key={index}>
+            <Image src={image} alt="slide_image" />
+          </SwiperSlide>
+        ))}
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <i class="fa-solid fa-chevron-left"></i>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <i class="fa-solid fa-chevron-right"></i>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Activity;
