a30bf5e945b3d38de4a4850fd7c70bf2182039e2
Phumiphat Matakeaw <phumiphat.mtk@gmail.com>
2024-02-06 01:18:57 +0700

    menu layout

--- src/shared/components/Menu.js
+++ src/shared/components/Menu.js
@@ -9,45 +9,77 @@ const Menu = () => {
   return (
     <div>
       <div className="grid grid-cols-4 gap-4 justify-items-center">
-        <div>
-          <button onClick={() => handleMeunuState(true, "deposit")}>
-            Deposit
+        <div className="text-center">
+          <button
+            onClick={() => handleMeunuState(true, "deposit")}
+            className="bg-[#d29517] p-2 rounded-full w-12 h-12 border-yellow-500 border-2 hover:bg-gradient-to-t from-yellow-200 to-orange-500"
+          >
+            <i className="fa-solid fa-plus text-3xl"></i>
           </button>
+          <div className="text-black">ฝากเงิน</div>
         </div>
-        <div>
-          <button onClick={() => handleMeunuState(true, "withdraw")}>
-            Withdraw
+        <div className="text-center">
+          <button
+            onClick={() => handleMeunuState(true, "withdraw")}
+            className="bg-[#d29517] p-2 rounded-full w-12 h-12 border-yellow-500 border-2 hover:bg-gradient-to-t from-yellow-200 to-orange-500"
+          >
+            <i className="fa-solid fa-minus text-3xl"></i>
           </button>
+          <div className="text-black">ถอนเงิน</div>
         </div>
-        <div>
-          <button onClick={() => handleMeunuState(true, "promotion")}>
-            Promotion
+        <div className="text-center">
+          <button
+            onClick={() => handleMeunuState(true, "promotion")}
+            className="bg-[#d29517] p-2 rounded-full w-12 h-12 border-yellow-500 border-2 hover:bg-gradient-to-t from-yellow-200 to-orange-500"
+          >
+            <i className="fa-solid fa-gift text-2xl"></i>
           </button>
+          <div className="text-black">โปรโมชั่น</div>
         </div>
-        <div>
-          <button onClick={() => handleMeunuState(true, "activity")}>
-            Activity
+        <div className="text-center">
+          <button
+            onClick={() => handleMeunuState(true, "activity")}
+            className="bg-[#d29517] p-2 rounded-full w-12 h-12 border-yellow-500 border-2 hover:bg-gradient-to-t from-yellow-200 to-orange-500"
+          >
+            <i className="fa-solid fa-calendar text-2xl"></i>
           </button>
+          <div className="text-black">กิจกรรม</div>
         </div>
-        <div>
-          <button onClick={() => handleMeunuState(true, "resetpassword")}>
-            Resetpassword
+        <div className="text-center">
+          <button
+            onClick={() => handleMeunuState(true, "resetpassword")}
+            className="bg-[#d29517] p-2 rounded-full w-12 h-12 border-yellow-500 border-2 hover:bg-gradient-to-t from-yellow-200 to-orange-500"
+          >
+            <i className="fa-solid fa-lock text-2xl"></i>
           </button>
+          <div className="text-black">เปลี่ยนรหัส</div>
         </div>
-        <div>
-          <button onClick={() => handleMeunuState(true, "record")}>
-            Record
+        <div className="text-center">
+          <button
+            onClick={() => handleMeunuState(true, "record")}
+            className="bg-[#d29517] p-2 rounded-full w-12 h-12 border-yellow-500 border-2 hover:bg-gradient-to-t from-yellow-200 to-orange-500"
+          >
+            <i className="fa-solid fa-clock-rotate-left text-2xl"></i>
           </button>
+          <div className="text-black">ประวัติ</div>
         </div>
-        <div>
-          <button onClick={() => handleMeunuState(true, "playgame")}>
-            Playgame
+        <div className="text-center">
+          <button
+            onClick={() => handleMeunuState(true, "playgame")}
+            className="bg-[#d29517] p-2 rounded-full w-12 h-12 border-yellow-500 border-2 hover:bg-gradient-to-t from-yellow-200 to-orange-500"
+          >
+            <i className="fa-solid fa-gamepad text-2xl"></i>
           </button>
+          <div className="text-black">เข้าเล่นเกม</div>
         </div>
-        <div>
-          <button onClick={() => handleMeunuState(true, "account")}>
-            Account
+        <div className="text-center">
+          <button
+            onClick={() => handleMeunuState(true, "account")}
+            className="bg-[#d29517] p-2 rounded-full w-12 h-12 border-yellow-500 border-2 hover:bg-gradient-to-t from-yellow-200 to-orange-500"
+          >
+            <i className="fa-solid fa-user text-2xl"></i>
           </button>
+          <div className="text-black">บัญชี</div>
         </div>
       </div>
     </div>
