8e9cbb7357c59d82a39fca62ac76a6e2bcd78261
Phumiphat Matakeaw <phumiphat.mtk@gmail.com>
2024-02-07 21:41:26 +0700

    game list

--- src/shared/components/PlayGame.js
+++ src/shared/components/PlayGame.js
@@ -1,9 +1,59 @@
-import React from 'react'
+"use client";
+
+import { useState } from "react";
 
 const PlayGame = () => {
-  return (
-    <div>PlayGame</div>
-  )
-}
+  const [gamesList, setGmameList] = useState(["sexy", "sa game", "wm"]);
+  const handleTab = (code) => {
+    setGmameList([]);
+    let list = [];
+    switch (code) {
+      case "casino":
+        list = ["sexy", "sa game", "wm"]; // api
+        setGmameList(list);
+        break;
+      case "slot":
+        list = ["JDB", "JOKER", "PG"];
+        setGmameList(list);
+        break;
+      case "game":
+        list = ["ป๊อก", "Joker finish ", "KingMaker"];
+        setGmameList(list);
+        break;
+      case "lotto":
+        list = ["lotto"];
+        setGmameList(list);
+        break;
+      default:
+        list = ["sexy", "sa game", "wm"];
+        setGmameList(list);
+        break;
+    }
+  };
 
-export default PlayGame
+  return (
+    <div className="px-5 mt-5">
+      <div className="flex flex-row items-center justify-center">
+        <div className="mr-5">
+          <button onClick={() => handleTab("casino")}>คาสิโน</button>
+        </div>
+        <div className="mr-5">
+          <button onClick={() => handleTab("slot")}>สล๊อต</button>
+        </div>
+        <div className="mr-5">
+          <button onClick={() => handleTab("game")}>เกม</button>
+        </div>
+        <div className="mr-5">
+          <button onClick={() => handleTab("lotto")}>หวย</button>
+        </div>
+      </div>
+      <div class="grid grid-cols-3 gap-3">
+        {gamesList.map((game) => (
+          <div key={game}>{game}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PlayGame;
